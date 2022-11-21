06ab82f6688cb2d94cb4951e04911fd0707aa6c7
Simon McMahon <simon@simonmc.net>
2022-11-21 22:35:17 +1300

    fix tests

--- test/test.cpp
+++ test/test.cpp
@@ -10,17 +10,21 @@ TEST(GPUCompute, TestNetwork) {
 	n.load_data();
 	GPUNetwork g;
 	g.init(n);
+	g.setup_calculate_only_pipeline(n);
 
 	LayerTrainingData expected(n.layers);
 	n.calculate(n.training_data[0].data, &expected);
-	std::vector<float> result;
-	g.calculate(n.training_data[0].data, result);
+	std::vector<float> output;
+	std::vector<float> activations;
+	std::vector<float> deltas;
+
+	g.calculate({ &n.training_data[0].data, &n.training_data[0].expected, &output, &activations, &deltas });
 
 	int ri = 0;
 	for (int l = 0; l < n.layers.size(); l++) {
 		auto o = expected.get_full_output(l);
 		for (int i = 0; i < n.layers[l].size; i++) {
-			EXPECT_FLOAT_EQ(result[ri++], o[i]);
+			EXPECT_FLOAT_EQ(output[ri++], o[i]);
 		}
 	}
 
@@ -34,19 +38,23 @@ TEST(GPUCompute, MNISTNetwork) {
 	n.load_data();
 	GPUNetwork g;
 	g.init(n);
+	g.setup_calculate_only_pipeline(n);
 
 	uint32_t di = 0;
 	for (auto d : n.training_data) {
 		LayerTrainingData expected(n.layers);
 		n.calculate(d.data, &expected);
-		std::vector<float> result;
-		g.calculate(d.data, result);
+		std::vector<float> output;
+		std::vector<float> activations;
+		std::vector<float> deltas;
+
+		g.calculate({ &d.data, &d.expected, &output, &activations, &deltas });
 
 		int ri = 0;
 		for (int l = 0; l < n.layers.size(); l++) {
 			auto o = expected.get_full_output(l);
 			for (int i = 0; i < n.layers[l].size; i++) {
-				ASSERT_NEAR(result[ri++], o[i], 0.0001) << "index " << i << " on case " << di;
+				ASSERT_NEAR(output[ri++], o[i], 0.0001) << "index " << i << " on case " << di;
 			}
 		}
 		di++;
